c0d4649d3b3b7c3cc3affd970bc45951514ea2e4
John Kwong <johnkwong@Johns-MacBook-Pro-2.local>
2017-11-28 23:21:25 -0800

    Added training script.

---
+++ src/main.py
@@ -0,0 +1,137 @@
+"""
+Train cat dog classifier
+
+
+
+
+"""
+
+import os, sys
+import numpy as np
+
+data_root_path = r'/Users/johnkwong/data'
+
+data_path = os.path.join(data_root_path, 'kaggle_cat_dog')
+
+training_data_path = os.path.join(data_path, 'train')
+test_data_path = os.path.join(data_path, 'test1')
+validation_data_path = os.path.join(data_path, 'validation')
+
+
+
+from keras.preprocessing.image import ImageDataGenerator
+
+datagen = ImageDataGenerator(
+        rotation_range=40,
+        width_shift_range=0.2,
+        height_shift_range=0.2,
+        rescale=1./255,
+        shear_range=0.2,
+        zoom_range=0.2,
+        horizontal_flip=True,
+        fill_mode='nearest')
+
+
+
+from keras.preprocessing.image import ImageDataGenerator, array_to_img, img_to_array, load_img
+
+datagen = ImageDataGenerator(
+        rotation_range=40,
+        width_shift_range=0.2,
+        height_shift_range=0.2,
+        shear_range=0.2,
+        zoom_range=0.2,
+        horizontal_flip=True,
+        fill_mode='nearest')
+
+img = load_img(os.path.join(training_data_path, 'cat.0.jpg'))  # this is a PIL image
+x = img_to_array(img)  # this is a Numpy array with shape (3, 150, 150)
+x = x.reshape((1,) + x.shape)  # this is a Numpy array with shape (1, 3, 150, 150)
+
+# the .flow() command below generates batches of randomly transformed images
+# and saves the results to the `preview/` directory
+i = 0
+for batch in datagen.flow(x, batch_size=1,
+                          save_to_dir=os.path.join(data_path, 'preview'), save_prefix='cat', save_format='jpeg'):
+    i += 1
+    if i > 20:
+        break  # otherwise the generator would loop indefinitely
+
+
+
+
+from keras.models import Sequential
+from keras.layers import Conv2D, MaxPooling2D
+from keras.layers import Activation, Dropout, Flatten, Dense
+
+model = Sequential()
+# model.add(Conv2D(32, (3, 3), input_shape=(3, 150, 150)))
+model.add(Conv2D(32, 3, 3, input_shape=(150, 150,3)))
+
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(Conv2D(32, (3, 3)))
+# model.add(Conv2D(32, 3, 3))
+
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(Conv2D(64, (3, 3)))
+# model.add(Conv2D(64, 3, 3))
+
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+# the model so far outputs 3D feature maps (height, width, features)
+
+################
+
+model.add(Flatten())  # this converts our 3D feature maps to 1D feature vectors
+model.add(Dense(64))
+model.add(Activation('relu'))
+model.add(Dropout(0.5))
+model.add(Dense(1))
+model.add(Activation('sigmoid'))
+
+model.compile(loss='binary_crossentropy',
+              optimizer='rmsprop',
+              metrics=['accuracy'])
+
+
+batch_size = 16
+
+# this is the augmentation configuration we will use for training
+train_datagen = ImageDataGenerator(
+        rescale=1./255,
+        shear_range=0.2,
+        zoom_range=0.2,
+        horizontal_flip=True)
+
+# this is the augmentation configuration we will use for testing:
+# only rescaling
+test_datagen = ImageDataGenerator(rescale=1./255)
+
+# this is a generator that will read pictures found in
+# subfolders of 'data/train', and indefinitely generate
+# batches of augmented image data
+train_generator = train_datagen.flow_from_directory(
+        training_data_path,  # this is the target directory
+        target_size=(150, 150),  # all images will be resized to 150x150
+        batch_size=batch_size,
+        class_mode='binary')  # since we use binary_crossentropy loss, we need binary labels
+
+# this is a similar generator, for validation data
+validation_generator = test_datagen.flow_from_directory(
+        validation_data_path,
+        target_size=(150, 150),
+        batch_size=batch_size,
+        class_mode='binary')
+
+model.fit_generator(
+        train_generator,
+        steps_per_epoch=2000 // batch_size,
+        epochs=50,
+        validation_data=validation_generator,
+        validation_steps=800 // batch_size)
+model.save_weights('cat_dog_classifier.h5')  # always save your weights after training or during training
